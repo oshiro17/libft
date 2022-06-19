ab34659510f2ebd0652449579cd9fe084e42586f
Nonoka Oshiro <noshiro@c1r30s3.42tokyo.jp>
2022-06-19 13:19:24 +0900

    add main

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pantti <pantti@student.42.fr>              +#+  +:+       +#+        */
+/*   By: noshiro <noshiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 17:10:12 by noshiro           #+#    #+#             */
-/*   Updated: 2022/06/19 07:30:54 by pantti           ###   ########.fr       */
+/*   Updated: 2022/06/19 13:18:19 by noshiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,17 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		dst[idx] = '\0';
 	return (ft_strlen(src));
 }
-
-// copies up to dstsize - 1 characters from the string src to dst, NUL-terminating the result if dstsize is not 0.
+// #include<stdio.h>
+// #include <string.h>
+// int main(void)
+// {
+// 	char dst [] = "12345";
+// 	char src[] = "coucou";
+// 	printf("%d",ft_strlcpy(dst,NULL,8));
+// 	printf("%s",dst);
+// 	return(0);
+// }
+// copies up to dstsize - 1 characters from the string src to dst,
+// NUL-terminating the result if dstsize is not 0.
+//dstがNULLの時本家はsegmentation fault
+//srcがNULLの時本家はabort

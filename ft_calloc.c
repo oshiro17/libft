29fc74c875484d21a6b4f78d12d1b576523e4eb3
Nonoka Oshiro <noshiro@c1r30s3.42tokyo.jp>
2022-06-19 09:22:55 +0900

    ft_bzero.の二つ目の引数は文字数

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: noshiro <noshiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 15:23:55 by noshiro           #+#    #+#             */
-/*   Updated: 2022/06/14 19:20:34 by noshiro          ###   ########.fr       */
+/*   Updated: 2022/06/19 09:14:43 by noshiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, nmemb * size);
+	ft_bzero(ptr, nmemb);
 	return (ptr);
 }
 

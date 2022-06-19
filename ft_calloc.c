5295b6d210bd041e2c52a6f8167a5a382dbe5c49
Nonoka Oshiro <noshiro@c1r30s3.42tokyo.jp>
2022-06-19 14:29:52 +0900

    size_t型の範囲を超えたときのエラー処理をした

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,22 +6,32 @@
 /*   By: noshiro <noshiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 15:23:55 by noshiro           #+#    #+#             */
-/*   Updated: 2022/06/19 09:14:43 by noshiro          ###   ########.fr       */
+/*   Updated: 2022/06/19 14:25:10 by noshiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	void	*ptr;
+	void	*s1;
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	if (count > SIZE_MAX / size)
 		return (NULL);
-	ft_bzero(ptr, nmemb);
-	return (ptr);
+	s1 = malloc(count * size);
+	if (!s1)
+		return (NULL);
+	ft_bzero(s1, count * size);
+	return (s1);
 }
-
-//nmemb * sizesize_tの範囲を超えてしまってる時、
-//mallocに任しちゃう
+// // nmemb * sizesize_tの範囲を超えてしまってる時、
+// // mallocに任しちゃう
+// #include <stdlib.h>
+// #include <stdio.h>
+// int main(void)
+// {
+// 	void	*ptr;
+// 	ptr = calloc(SIZE_MAX, SIZE_MAX);
+// 	printf("%p",ptr);
+// 	return(0);
+// }

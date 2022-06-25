451cee62ba74306ffa1fa3601f4776a84c217a59
Nonoka Oshiro <noshiro@c1r27s5.42tokyo.jp>
2022-06-25 15:54:04 +0900

    LONG_MAX超える値が来たときの処理

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: noshiro <noshiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 15:41:05 by noshiro           #+#    #+#             */
-/*   Updated: 2022/06/19 21:10:42 by noshiro          ###   ########.fr       */
+/*   Updated: 2022/06/25 15:26:37 by noshiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,38 @@
 
 int	ft_atoi(const char *str)
 {
-	int		sign;
-	int		ret;
+	int				sign;
+	unsigned long	num;
 
-	ret = 0;
-	sign = -1;
+	sign = 1;
+	num = 0;
 	while (*str == ' ' || *str == '\n' || *str == '\t' || *str == '\v'
 		|| *str == '\f' || *str == '\r')
 		str++;
 	if (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-			sign = 1;
-		str++;
-	}
+		if (*str++ == '-')
+			sign = -1;
 	while (*str >= '0' && *str <= '9')
 	{
-		ret = (ret * 10) - ((int)*str - '0');
+		num = (num * 10) + ((int)*str - '0');
 		str++;
+		if ((sign > 0 && num > LONG_MAX) || \
+		(sign < 0 && num > (LONG_MAX + 1UL)))
+		{
+			if (sign > 0)
+				return (-1);
+			return (0);
+		}
 	}
-	return (ret * sign);
+	return (num * sign);
 }
 
 // // intの範囲を超える時は回っちゃう
 // #include "stdio.h"
 // #include "string.h"
-
 // int main()
 // {
-// 	printf("original :%d\n", atoi("29387294"));
-// 	printf("nonoka :%d\n", ft_atoi("29387294"
-// 	));
+// 	printf("original :%d\n", atoi("2147483648"));
+// 	printf("nonoka :%d\n", ft_atoi("2147483648"));
 // 	return 0;
 // }

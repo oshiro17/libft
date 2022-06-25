db43f5c72e09dd3ce15eafaaf7da5397bf35f6d0
Nonoka Oshiro <noshiro@c1r27s5.42tokyo.jp>
2022-06-25 17:22:04 +0900

    システムコール良くないのでかきけたでござるｷｬ━━━━(ﾟ∀ﾟ)━━━━cat ft_putstr_fd.c　

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pantti <pantti@student.42.fr>              +#+  +:+       +#+        */
+/*   By: noshiro <noshiro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 00:21:56 by pantti            #+#    #+#             */
-/*   Updated: 2022/06/17 00:27:04 by pantti           ###   ########.fr       */
+/*   Updated: 2022/06/25 17:10:33 by noshiro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	size_t	i;
-
-	i = 0;
 	if (!s)
 		return ;
-	while (s[i])
-	{
-		ft_putchar_fd(s[i], fd);
-		i++;
-	}
+	write(fd, s, ft_strlen(s));
 }
+
+// // s = "Hello"
+// // ft_putchar_fd => write H
+// // ft_putchar_fd => write e
+// // ft_putchar_fd => write l
+// // ft_putchar_fd => write l
+// // ft_putchar_fd => write o
+// // write Hello
+// int main (void)
+// {
+// 	ft_putstr_fd("aiueo",1);
+// 	return(0);
+// }

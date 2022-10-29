0e378e9927b70bd8e7bd1e7a4fd19d2187582df0
oshro17 <nonokapiano@icloud.com>
2022-10-29 17:16:44 +0900

    header add

--- libft.h
+++ libft.h
@@ -12,9 +12,9 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
+#include <stdio.h>
 // # include <stdio.h>
-// # include <string.h>
+# include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
 typedef struct	s_list
@@ -61,4 +61,17 @@ void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 void				ft_striteri(char *s, void (*f)(unsigned int, char*));
 char				*ft_strtrim(char const	*s1, char const *set);
+t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
+void				ft_lstdelone(t_list *lst, void (*del)(void
+*));
+void				ft_lstclear(t_list **lst, void (*del)(void
+*));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
+void (*del)(void *));
+
 #endif
